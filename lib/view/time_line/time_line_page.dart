d41982bbb3d2248520656375e3661ac0e0124202
MakotoTaguchi <taguchimakoto@icloud.com>
2022-07-21 17:34:20 +0900

    posrpage作成

--- lib/view/time_line/time_line_page.dart
+++ lib/view/time_line/time_line_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:udemy_flutter/model/account.dart';
 import 'package:udemy_flutter/model/post.dart';
+import 'package:udemy_flutter/view/time_line/post_page.dart';
 
 class TimeLinePage extends StatefulWidget {
   const TimeLinePage({Key? key}) : super(key: key);
@@ -37,68 +38,76 @@ class _TimeLinePageState extends State<TimeLinePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            'タイムライン',
-            style: TextStyle(color: Colors.black),
-          ),
-          backgroundColor: Theme.of(context).canvasColor,
-          elevation: 2,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'タイムライン',
+          style: TextStyle(color: Colors.black),
         ),
-        body: ListView.builder(
-            itemCount: postList.length,
-            itemBuilder: (context, index) {
-              return Container(
-                decoration: BoxDecoration(
-                    border: index == 0
-                        ? Border(
-                            top: BorderSide(color: Colors.grey, width: 0),
-                            bottom: BorderSide(color: Colors.grey, width: 0),
-                          )
-                        : Border(
-                            bottom: BorderSide(color: Colors.grey, width: 0),
-                          )),
-                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 22,
-                      foregroundImage: NetworkImage(myAccount.imagePath),
-                    ),
-                    Expanded(
-                      child: Container(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  children: [
-                                    Text(
-                                      myAccount.name,
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    Text(
-                                      '@${myAccount.userId}',
-                                      style: TextStyle(color: Colors.grey),
-                                    ),
-                                  ],
-                                ),
-                                Text(DateFormat('M/d/yy')
-                                    .format(postList[index].createdTime!))
-                              ],
-                            ),
-                            Text(postList[index].content)
-                          ],
-                        ),
+        backgroundColor: Theme.of(context).canvasColor,
+        elevation: 2,
+      ),
+      body: ListView.builder(
+          itemCount: postList.length,
+          itemBuilder: (context, index) {
+            return Container(
+              decoration: BoxDecoration(
+                  border: index == 0
+                      ? Border(
+                          top: BorderSide(color: Colors.grey, width: 0),
+                          bottom: BorderSide(color: Colors.grey, width: 0),
+                        )
+                      : Border(
+                          bottom: BorderSide(color: Colors.grey, width: 0),
+                        )),
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    radius: 22,
+                    foregroundImage: NetworkImage(myAccount.imagePath),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    myAccount.name,
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(
+                                    '@${myAccount.userId}',
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                ],
+                              ),
+                              Text(DateFormat('M/d/yy')
+                                  .format(postList[index].createdTime!))
+                            ],
+                          ),
+                          Text(postList[index].content)
+                        ],
                       ),
-                    )
-                  ],
-                ),
-              );
-            }));
+                    ),
+                  )
+                ],
+              ),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => PostPage()));
+        },
+        child: Icon(Icons.chat_bubble_outline),
+      ),
+    );
   }
 }
