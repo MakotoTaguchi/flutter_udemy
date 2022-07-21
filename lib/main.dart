640afb34038e3df0d1f43133f492240d037e640a
MakotoTaguchi <taguchimakoto@icloud.com>
2022-07-21 18:10:55 +0900

    bottomnavigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_flutter/view/screen.dart';
 import 'package:udemy_flutter/view/time_line/time_line_page.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: TimeLinePage(),
+      home: Screen(),
     );
   }
 }
