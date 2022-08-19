b5f53f44a75764d55af5b223b85cb614fc540295
MakotoTaguchi <taguchimakoto@icloud.com>
2022-08-19 22:54:42 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-// import 'package:udemy_flutter/view/screen.dart';
+import 'package:udemy_flutter/view/screen.dart';
 import 'package:udemy_flutter/view/start_up/login_page.dart';
 import 'package:udemy_flutter/view/time_line/time_line_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
